acbe0e0907122471a140938fd7738aea7abc6748
Yanislav Georgiev <stu2101261071@gmail.com>
2022-07-11 13:35:16 +0300

    First Changes

---
+++ Learning ADO/Learning ADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_ADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
